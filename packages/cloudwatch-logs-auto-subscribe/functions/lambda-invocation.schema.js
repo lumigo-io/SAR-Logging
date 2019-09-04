f61b101c0301d3675e1a54183a345d5023036f74
theburningmonk <theburningmonk@gmail.com>
2019-09-04 10:45:24 +0200

    refactor: switch to apache 2

--- packages/cloudwatch-logs-auto-subscribe/functions/lambda-invocation.schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("@hapi/joi");
-
-const LambdaInvocation = Joi.object().keys({
-	FunctionName: Joi.string().required(),
-	Payload: Joi.object()
-});
-
-module.exports = LambdaInvocation;
